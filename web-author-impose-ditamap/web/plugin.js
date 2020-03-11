7a32149f4290c5668110cc1c68b78bf49571c24a
Cristian Talau <cristi_talau@sync.ro>
2020-03-11 15:08:28 +0200

    Call updateContext instead of modifying the context directly.

--- web-author-impose-ditamap/web/plugin.js
+++ web-author-impose-ditamap/web/plugin.js
@@ -1,16 +1,21 @@
 (function() {
-  var config = window.ditaMapConfig;
-  let ditaContext = workspace.getEditingContextManager().getDitaContext();
+  let config = window.ditaMapConfig;
+  let editingContextManager = workspace.getEditingContextManager();
+  let ditaContext = editingContextManager.getDitaContext();
   if (config && !ditaContext.ditamapUrl) {
+    let newDitaMapUrl = null;
     let singleMapQueryString = config.singleDITAMap && config.singleDITAMap.queryString;
     if (singleMapQueryString) {
-      ditaContext.ditamapUrl = new URLSearchParams(singleMapQueryString).get('url');
+      newDitaMapUrl = new URLSearchParams(singleMapQueryString).get('url');
     } else {
-      var ditaMapFileName = config.singleTopicLayout && config.singleTopicLayout.ditaMapFileName;
+      let ditaMapFileName = config.singleTopicLayout && config.singleTopicLayout.ditaMapFileName;
       if (ditaMapFileName) {
         let url = new URLSearchParams(window.location.search).get('url');
-        ditaContext.ditamapUrl = url.replace(new RegExp('/topics/.*'), '/' + ditaMapFileName);
+        newDitaMapUrl = url.replace(new RegExp('/topics/.*'), '/' + ditaMapFileName);
       }
+    }
+    if (newDitaMapUrl !== null) {
+      editingContextManager.updateDitaContext(new sync.api.DitaContext(newDitaMapUrl, ditaContext.filterUrl));
     }
   } else {
     // The DITA Map is already configured using the `ditamap` URL parameter.
