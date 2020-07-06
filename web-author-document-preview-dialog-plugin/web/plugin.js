ffa7192ee24a99e086c4b1f1073125e18e833f34
Cristian Talau <cristi_talau@sync.ro>
2020-07-06 20:28:37 +0300

    Simplified code.

--- web-author-document-preview-dialog-plugin/web/plugin.js
+++ web-author-document-preview-dialog-plugin/web/plugin.js
@@ -1,59 +1,11 @@
 (function () {
   /**
-   * Renderer that will show the preview for a given URL.
-   */
-  class DocumentPreviewRenderer {
-    constructor(editor) {
-      this.editor = editor;
-      this.previewElement = goog.dom.createDom('div', 'oxy-document-preview');
-      this.previewElement.style.boxSizing = "border-box";
-      this.documentUrl = null;
-    }
-
-    /**
-     * @return {Element} The element where the documents are previewed.
-     */
-    getPreviewElement() {
-      return this.previewElement;
-    }
-
-    /**
-     * Preview the given URL.
-     * @param {string} url URL of document to be previewed.
-     */
-    previewDocument(url) {
-      if (this.documentUrl !== url) {
-        this.clear();
-        this.documentUrl = url;
-        this.editor.previewAuthorDocument(url, this.previewElement)
-          .then(goog.bind(function(editingSupport) {
-            this.editingSupport_ = editingSupport;
-        }, this)).thenCatch(function(e) {console.error(e)});
-      }
-    }
-
-    /**
-     * Clear the preview.
-     */
-    clear() {
-      this.documentUrl = null;
-      goog.dom.removeChildren(this.previewElement);
-      if (this.editingSupport_) {
-        this.editingSupport_.dispose();
-        this.editingSupport_.getActionsManager().dispose();
-      }
-    }
-  }
-
-
-  /**
-   * The action that preview documents inside a dialog.
+   * The action that previews documents inside a dialog.
    */
   class ShowDocumentPreviewAction extends sync.actions.AbstractAction {
     constructor(editor) {
       super();
       this.editor = editor;
-      this.previewRenderer_ = new DocumentPreviewRenderer(editor);
     }
 
     /** @override */
@@ -63,114 +15,57 @@
 
     /** @override */
     actionPerformed(callback) {
-      this.callback_ = callback || goog.nullFunction;
-      if (!this.isEnabled()) {
-        this.callback_();
-        return;
-      }
-
-      this.showFileBrowser();
+      let context = new sync.api.UrlChooser.Context(sync.api.UrlChooser.Type.EXTERNAL_REF);
+      workspace.getUrlChooser().chooseUrl(context, url => {
+        if (url) {
+          this.showPreviewDocumentDialog(url, callback);
+        } else {
+          callback();
+        }
+      }, sync.api.UrlChooser.Purpose.CHOOSE);
     };
 
-    /**
-     * Show the file browser.
-     */
-    showFileBrowser() {
-      this.previewRenderer_.clear();
-      this.previewDialog_ && this.previewDialog_.hide();
-
-      var urlChooser = workspace.getUrlChooser();
-      var context = new sync.api.UrlChooser.Context(sync.api.UrlChooser.Type.EXTERNAL_REF);
-      urlChooser.chooseUrl(context, goog.bind(this.onFileChoosen, this), sync.api.UrlChooser.Purpose.CHOOSE);
-    };
-
-    /**
-     * Called when the user choose a document.
-     * @param {string} url The choosen URL.
-     */
-    onFileChoosen(url) {
-      if (url) {
-        this.documentUrl = url;
-        this.createPreviewDialog();
-        this.showPreviewDocumentDialog(url);
-      }
-    };
 
     /**
      * Show the preview dialog.
      * @param {string} url The URL to be previewed.
+     * @param {function} callback The action callback.
      */
-    showPreviewDocumentDialog(url) {
-      this.previewRenderer_.previewDocument(url);
-      this.previewDialog_.show();
+    showPreviewDocumentDialog(url, callback) {
+      let dialog = workspace.createDialog();
+      dialog.setPreferredSize(750, 700);
+      dialog.setResizable(true);
+      dialog.setTitle('Preview');
+
+      let editingSupport = null;
+      dialog.onSelect(() => {
+        callback();
+        editingSupport.dispose();
+        editingSupport.getActionsManager().dispose();
+        dialog.dispose()
+      });
+
+      dialog.getElement().classList.add('oxy-document-preview');
+      this.editor.previewAuthorDocument(url, dialog.getElement())
+          .then(es => editingSupport = es)
+          .thenCatch(console.error);
+
+      dialog.show();
     };
-
-    /**
-     * Called when the preview dialog is closed.
-     * @param {string} key The close key.
-     * @param {object} e
-     */
-    onPreviewDialogClosed(key, e) {
-      this.previewRenderer_.clear();
-      if (key === 'ok') {
-        console.log("Document URL: ", this.documentUrl);
-      } else {
-        this.callback_();
-      }
-    }
-
-    /**
-     * Create the preview dialog.
-     */
-    createPreviewDialog() {
-      if (!this.previewDialog_) {
-        var cD = goog.dom.createDom;
-
-        var dialog = workspace.createDialog();
-        this.previewDialog_ = dialog;
-        dialog.setPreferredSize(750, 700);
-        dialog.setResizable(true);
-        dialog.setTitle(this.dialogTitle_);
-        dialog.setButtonConfiguration(sync.api.Dialog.ButtonConfiguration.OK_CANCEL);
-
-        goog.dom.append(
-          dialog.getElement(),
-          cD('div', 'content-wrapper',
-            this.previewRenderer_.getPreviewElement()
-          )
-        );
-
-        this.previewDialog_.onSelect(goog.bind(this.onPreviewDialogClosed, this));
-      }
-    };
-  }
-
-  /**
-   * @param {object} actionsConfiguration The actions configuration.
-   * @return {boolean} true if the given actions loaded come from the framework.
-   */
-  function isFrameworkActions(actionsConfiguration) {
-    var toolbars = actionsConfiguration.toolbars;
-    for(var i = 0 ; i < toolbars.length; i++){
-      if(toolbars[i].name !== "Builtin" && toolbars[i].name !== "Review") {
-        return true;
-      }
-    }
-    return false;
   }
 
   /**
    * Add the preview action to toolbar.
    */
-  goog.events.listen(workspace, sync.api.Workspace.EventType.BEFORE_EDITOR_LOADED, function(e) {
-    var editor = e.editor;
-
-    var actionId = 'choose.link.target.action';
+  goog.events.listen(workspace, sync.api.Workspace.EventType.EDITOR_LOADED, function(e) {
+    let editor = e.editor;
+    let actionId = 'choose.link.target.action';
     editor.getActionsManager().registerAction(actionId, new ShowDocumentPreviewAction(editor));
 
-    goog.events.listen(editor, sync.api.Editor.EventTypes.ACTIONS_LOADED, function(e) {
-      if(isFrameworkActions(e.actionsConfiguration)) {
-        e.actionsConfiguration.toolbars[0].children.push({
+    goog.events.listen(editor, sync.api.Editor.EventTypes.ACTIONS_LOADED, e => {
+      let toolbar = e.actionsConfiguration.toolbars[0];
+      if (toolbar && toolbar.name === 'Builtin') {
+        toolbar.children.push({
           id: actionId,
           type: 'action'
         });
