008638478ec8c0135fa9eb6d1da0e5bea635fa78
Cristian Talau <cristi_talau@sync.ro>
2021-04-13 18:24:51 +0300

    Use a more flexible match

--- web-author-comment-eol-fixup/wsAccess.js
+++ web-author-comment-eol-fixup/wsAccess.js
@@ -1,7 +1,7 @@
 function applicationStarted(pluginWorkspaceAccess) {
   var fixNewLines = new JavaAdapter(Packages.ro.sync.ecss.extensions.api.AuthorDocumentFilter, {
     addCommentMarker: function(filterBypass, startOffset, endOffset, comment, parentID) {
-        filterBypass.addCommentMarker(startOffset, endOffset, comment.replaceAll("\r\n", "\n"), parentID);
+        filterBypass.addCommentMarker(startOffset, endOffset, comment.replace(new RegExp("\r?\n", "g"), "\n"), parentID);
     }
   });
   pluginWorkspaceAccess.addEditingSessionLifecycleListener(
