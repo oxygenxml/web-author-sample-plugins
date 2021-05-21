2fe85fa5ade6f508eb20afc5f6a592669b9dcf09
Cristian Talau <cristi_talau@sync.ro>
2021-05-21 14:13:56 +0300

    Add plugin to open a new file

---
+++ web-author-file-open-action/README.md
@@ -0,0 +1,4 @@
+Web Author File Open Action plugin
+==================================
+
+Oxygen XML Web Author plugin that contributes an "Open..." action to the "More..." (triple dots) menu that lets the user browse for a file and open it in the current editor.
---
+++ web-author-file-open-action/web/plugin.js
@@ -0,0 +1,23 @@
+workspace.listen(sync.api.Workspace.EventType.BEFORE_EDITOR_LOADED, function(e) {
+  var editor = e.editor;
+  class FileOpenAction extends sync.actions.AbstractAction {
+
+    getDisplayName() {
+      return 'Open...';
+    }
+
+    actionPerformed(callback) {
+      var context = new sync.api.UrlChooser.Context(sync.api.UrlChooser.Type.GENERIC);
+      var target = sync.api.Editor.LinkOpenedEvent.Target.BLANK;
+      workspace.getUrlChooser().chooseUrl(context, url => {
+        editor.openLink(new sync.api.Editor.LinkOpenedEvent(url, false, null, target);
+      }, sync.api.UrlChooser.Purpose.CHOOSE);
+      callback();
+    }
+  }
+  editor.getActionsManager().registerAction('file.open', new FileOpenAction());
+  
+  editor.listen(sync.api.Editor.EventTypes.ACTIONS_LOADED, function(e) {
+    e.actionsConfiguration.appendToToolbar('file.open', 'Builtin');
+  });
+});
