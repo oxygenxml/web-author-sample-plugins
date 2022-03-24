5209c4ead851ff16db7697b052420ae8c30e6834
Mihaela Calotescu <mihaela@oxygenxml.com>
2022-03-24 16:22:57 +0200

    WA-5627 Added a plugin that adds the outputclass attribute to paragraph elements, when inserted in the document.

---
+++ web-author-set-attribute-on-new-paragraphs/README.md
@@ -0,0 +1,4 @@
+Add output class to paragraph fragments
+===============================
+
+Plugin that adds the outputclass attribute to paragraph elements, when inserted in the document. 
---
+++ web-author-set-attribute-on-new-paragraphs/wsAccess.js
@@ -0,0 +1,25 @@
+function applicationStarted(pluginWorkspaceAccess) {
+  pluginWorkspaceAccess.addEditingSessionLifecycleListener(
+      new JavaAdapter(Packages.ro.sync.ecss.extensions.api.webapp.access.WebappEditingSessionLifecycleListener, {
+        editingSessionStarted: function (docId, authorDocumentModel) {
+          var adapter = new JavaAdapter(Packages.ro.sync.ecss.extensions.api.AuthorDocumentFilter, {
+            insertFragment: function(bypass, offset, frag) {
+              var contentNodes = frag.getContentNodes();
+              var nodesCount = contentNodes.size();
+              for (var i = 0; i <nodesCount; i++) {
+                var node = contentNodes.get(i);
+                if (node instanceof Packages.ro.sync.ecss.extensions.api.node.AuthorElement && "p" == node.getName()) {
+                  node.setAttribute("outputclass", new Packages.ro.sync.ecss.extensions.api.node.AttrValue("normal"));
+                }
+              }
+              return bypass.insertFragment(offset, frag);
+            }
+          });
+          authorDocumentModel.getAuthorAccess().getDocumentController().setDocumentFilter(adapter);
+        }
+      }));
+}
+
+function applicationClosing(pluginWorkspaceAccess) {
+  // Nothing to do.
+}
