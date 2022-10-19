c3ec8b28bfd6a2efe89c3d683e1c83f63c47f048
Cristian Talau <cristitalau@gmail.com>
2022-10-19 14:37:37 +0300

    WA-5983: Remove the App component.

--- web-author-react-form-control/js/HorizontalStepper.js
+++ web-author-react-form-control/js/HorizontalStepper.js
@@ -4,18 +4,18 @@ import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
 import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 
-const steps = ['Select campaign settings', 'Create an ad group', 'Create an ad'];
-
-export default function HorizontalLinearStepper() {
+export default function HorizontalLinearStepper({steps, onStepChanged}) {
   const [activeStep, setActiveStep] = React.useState(0);
   const handleNext = () => {
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    onStepChanged(activeStep);
   };
   const handleBack = () => {
     setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    onStepChanged(activeStep);
   };
+
   return (
       <Box sx={{ width: '100%' }}>
         <Stepper activeStep={activeStep}>
@@ -25,7 +25,6 @@ export default function HorizontalLinearStepper() {
                 </Step>
           )}
         </Stepper>
-        <Typography sx={{ mt: 2, mb: 1 }}>Steps {activeStep + 1}</Typography>
         <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
           <Button color="inherit" onClick={handleBack} disabled={activeStep === 0}>
             Back
