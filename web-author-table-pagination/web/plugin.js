e774cbfddb171f319eb9945ec99c50e1b3c02f8a
Bina Mircea <mircea_bina@sync.ro>
2025-03-20 11:06:09 +0200

    WA-8362: Disable table drag handler for performance

--- web-author-table-pagination/web/plugin.js
+++ web-author-table-pagination/web/plugin.js
@@ -2,3 +2,9 @@ workspace.listen(sync.api.Workspace.EventType.BEFORE_EDITOR_LOADED, function(e) 
   e.editor.registerEnhancer(
       'com.oxygenxml.web_author.pagination.TablePaginationRenderer', TablePagination);
 });
+
+
+// Disable table drag handler for performance
+workspace.listen(sync.api.Workspace.EventType.BEFORE_EDITOR_LOADED, (e) => {
+  sync.table.TableDragHandleInstaller.prototype.attach = () => {};
+});
