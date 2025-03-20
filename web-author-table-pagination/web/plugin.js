0602668f603c978c63bacdfb35442b2a788d8b8f
Bina Mircea <mircea_bina@sync.ro>
2025-03-20 15:01:56 +0200

    WA-8362: Add stripe to suggest no-tags mode

--- web-author-table-pagination/web/plugin.js
+++ web-author-table-pagination/web/plugin.js
@@ -8,3 +8,29 @@ workspace.listen(sync.api.Workspace.EventType.BEFORE_EDITOR_LOADED, function(e) 
 workspace.listen(sync.api.Workspace.EventType.BEFORE_EDITOR_LOADED, (e) => {
   sync.table.TableDragHandleInstaller.prototype.attach = () => {};
 });
+
+workspace.listen(sync.api.Workspace.EventType.EDITOR_LOADED, (e) => {
+  let isTagsForced = !!e.editor.options["tags-mode"];
+  if (!isTagsForced && workspace.getOption(sync.OptionTags.SENTINELS_DISPLAY_MODE) !== 'no-tags') {
+    
+    // Create an editor stripe with height 27px
+    let stripe = workspace.createEditorStripe(27);
+    stripe.classList.add('flash-message-warn', 'vertical-align-children');
+
+    // Create and append the text message
+    let message = document.createElement('span');
+    message.textContent = 'Switch to no-tags mode for better performance when editing large tables:';
+    stripe.appendChild(message);
+
+    // Create and append the button
+    let button = document.createElement('button');
+    button.textContent = 'Switch to no-tags mode';
+    button.onclick = () => {
+      // Set the option to no-tags mode
+      workspace.setOption(sync.OptionTags.SENTINELS_DISPLAY_MODE, 'no-tags');
+      workspace.removeEditorStripe(stripe);
+    };
+    stripe.appendChild(button);
+
+  }
+});
