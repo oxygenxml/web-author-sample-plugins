0057fd5f238d6234f3a6915e2cd9cbe359af6b86
Cosmin Eftenie <138829898+cosmin-eftenie@users.noreply.github.com>
2025-09-12 17:13:13 +0300

    Add files via upload

---
+++ web-author-preview-dialog/README.md
@@ -0,0 +1,6 @@
+Preview Dialog
+==============
+
+This plugin adds a new action to the DITA toolbar that opens a preview dialog. 
+The dialog displays a sample XML fragment. 
+The XML fragment is styled according to the framework applied to the currently opened document.
---
+++ web-author-preview-dialog/web/preview-dialog.js
@@ -0,0 +1,100 @@
+class PreviewAction extends sync.actions.AbstractAction {
+  /** 
+   * @type {sync.api.Editor} editor The editor object.
+   */
+  constructor(editor) {
+    // Shortcut: Meta+L pe Mac, Ctrl+L pe alte platforme
+    super(editor, 'M1 L');
+    this.editor = editor;
+  }
+
+  /** @override */
+  getDisplayName() {
+    return 'Preview dialog';
+  }
+
+  /** @override */
+  isEnabled() {
+    // Acțiunea e mereu activă
+    return true;
+  }
+
+  /** @override */
+  actionPerformed(callback) {
+    if (!workspace.currentEditor) {
+      console.error("No current editor available");
+      callback && callback();
+      return;
+    }
+
+    // Creează dialogul de preview
+    const dialog = workspace.createDialog();
+    dialog.setPreferredSize(750, 700);
+    dialog.setResizable(true);
+    dialog.setTitle('Preview');
+
+    const fragmentXML = `
+<!DOCTYPE topic PUBLIC "-//OASIS//DTD DITA Topic//EN" "topic.dtd">
+<topic id="test">
+  <title>Sample fragment</title>
+  <body>
+    <p>Test preview</p>
+  </body>
+</topic>
+`;
+
+    workspace.currentEditor.loadPreview(dialog.getElement(), {
+      url: "http://placeholder.txt", 
+      content: fragmentXML
+    });
+
+    dialog.show();
+
+    callback && callback();
+  }
+}
+
+// Înregistrează acțiunea înainte de încărcarea editorului
+workspace.listen(sync.api.Workspace.EventType.BEFORE_EDITOR_LOADED, function(e) {
+  var editor = e.editor;
+  editor.getActionsManager().registerAction('preview.dialog', new PreviewAction(editor));
+  
+  addToDitaToolbar(editor, 'preview.dialog');
+});
+
+// Actualizează statusul acțiunii la schimbarea selecției
+workspace.listen(sync.api.Workspace.EventType.EDITOR_LOADED, function(e) {
+  var editor = e.editor;
+
+  editor.getSelectionManager().listen(
+    sync.api.SelectionManager.EventType.SELECTION_CHANGED,
+    function() {
+      editor.getActionsManager().refreshActionsStatus('preview.dialog');
+    }
+  );
+});
+
+// Funcție pentru adăugarea acțiunii în toolbar-ul DITA
+function addToDitaToolbar(editor, actionId) {
+  editor.listen(sync.api.Editor.EventTypes.ACTIONS_LOADED, function(e) {
+    var actionsConfig = e.actionsConfiguration;
+    var ditaToolbar = null;
+
+    if (actionsConfig.toolbars) {
+      for (var i = 0; i < actionsConfig.toolbars.length; i++) {
+        var toolbar = actionsConfig.toolbars[i];
+        if (toolbar.name === "DITA") {
+          ditaToolbar = toolbar;
+          break;
+        }
+      }
+    }
+
+    if (ditaToolbar) {
+      ditaToolbar.children.push({
+        id: actionId,
+        type: "action"
+      });
+    }
+  });
+}
